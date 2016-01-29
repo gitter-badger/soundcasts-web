28b3ea055e046665342a82a6ddffedd6afea4224
Ryan Tsao <rtsao@uber.com>
2016-01-28 22:49:22 -0800

    lol

--- squad.js
+++ squad.js
@@ -1,12 +1,60 @@
 const exec = require('child_process').exec;
 
-function ifNotDirty(cb) {
-  exec('git status --porcelain', function (error, stdout, stderr) {
-    error || stdout.length || stderr.length ?
-      console.log('dirty') : cb();
+// checkIfClean(function() {
+//   console.log('we are clean');
+// }, function() {
+//   console.log('we are dirty');
+// });
+
+
+
+/**
+ * Helpers
+ */
+
+function deploy(directory, branch) {
+
+  function pushCmd() {
+    return push(directory, branch);
+  }
+
+  checkIfClean()
+    .then(pushCmd, uncleanError);
+}
+
+function push(directory, branch, message) {
+  var cmd = [
+    'npm run ' + directory,
+    'git branch -D ' + branch,
+    'git --work-tree ' + directory + ' checkout --orphan ' + branch,
+    'git --work-tree ' + directory + ' add --all',
+    'git --work-tree ' + directory + ' commit -m "some commit message"',
+    'git push origin ' + branch + ' --force',
+  ].join(' && ');
+
+  return new Promise(function(resolve, reject) {
+    exec(cmd, function(error, stdout, stderr) {
+      if (error || stderr) {
+        reject();
+      } else {
+        resolve();
+      }
+    });
   });
 }
 
-ifNotDirty(function() {
-  console.log('we are not dirty');
-});
+function checkIfClean() {
+  return new Promise(function(resolve, reject) {
+    exec('git status --porcelain', function (error, stdout, stderr) {
+      var gitClean = !(error || stdout.length || stderr.length);
+
+      gitClean ? resolve('q') : reject('q');
+    });
+  });
+}
+
+function uncleanError() {
+  console.error('git unclean');
+}
+
+deploy('build', 'squad');
