98cc8223ba3dbc3c9a62b936bd994e71e1bde62d
Ryan Tsao <rtsao@uber.com>
2016-01-28 22:04:47 -0800

    swag

--- squad.js
+++ squad.js
@@ -1,29 +1,12 @@
-// var spawn = require('child_process').spawn;
-
-// var ls = spawn('git', ['status', '--porcelain']);
-
-// ls.stdout.on('data', (data) => {
-//   console.log(`stdout: ${data}`);
-// });
-
-// ls.stderr.on('data', (data) => {
-//   console.log(`stderr: ${data}`);
-// });
-
-// ls.on('close', (code) => {
-//   console.log(`child process exited with code ${code}`);
-// });
-
 const exec = require('child_process').exec;
 
-const child = exec('git status --porcelain',
-  (error, stdout, stderr) => {
+function ifNotDirty(cb) {
+  exec('git status --porcelain', function (error, stdout, stderr) {
+    error || stdout.length || stderr.length ?
+      console.log('dirty') : cb();
+  });
+}
 
-
-
-    console.log(stdout.length);
-    console.log(`stderr: ${stderr}`);
-    if (error !== null) {
-      console.log(`exec error: ${error}`);
-    }
+ifNotDirty(function() {
+  console.log('we are not dirty');
 });
